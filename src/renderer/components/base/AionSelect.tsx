486a0c6b6f55c0d0b34efe8218bb4c7f84b72c83
yunpeng.wang <wypwelldone@gmail.com>
2026-06-11 19:48:32 +0800

    chore(style): Arco 覆盖样式迁移为 SCSS，新增 sass-embedded 依赖

--- src/renderer/components/base/AionSelect.tsx
+++ src/renderer/components/base/AionSelect.tsx
@@ -26,8 +26,8 @@ export interface AionSelectProps extends NativeSelectProps {
 
 /**
  * 基础样式类名
- * 注意：主题相关样式（背景色、边框色）在 arco-override.css 的 .aion-select 类中定义
- * Note: Theme-related styles (background, border colors) are defined in .aion-select class in arco-override.css
+ * 注意：主题相关样式（背景色、边框色）在 styles/arco-override.scss 的 .aion-select 类中定义
+ * Note: Theme-related styles (background, border colors) are defined in .aion-select class in styles/arco-override.scss
  */
 const BASE_CLASS = classNames('aion-select', '[&_.arco-select-view]:rounded-[4px]', '[&_.arco-select-view]:border', '[&_.arco-select-view]:border-solid', '[&_.arco-select-view]:border-border-2', '[&_.arco-select-view]:shadow-none', '[&_.arco-select-view]:transition-colors', '[&_.arco-select-view:hover]:border-[var(--color-primary)]', '[&_.arco-select-view:focus-within]:border-[var(--color-primary)]', '[&_.arco-select-view-disabled]:bg-[var(--color-bg-2)]', '[&_.arco-select-view-disabled]:opacity-80');
 
@@ -85,7 +85,7 @@ const defaultGetPopupContainer = (): HTMLElement => {
  * </AionSelect>
  * ```
  *
- * @see arco-override.css for theme-related styles (.aion-select)
+ * @see styles/arco-override.scss for theme-related styles (.aion-select)
  */
 const mapSizeToNative = (size?: AionSelectSize): NativeSelectSize | undefined => {
   if (!size) return undefined;
